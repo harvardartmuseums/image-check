371e80cb5519d60551425dfd4e778f96ed81c64a
Ian Callahan <code@greatfortress.com>
2025-02-18 13:04:48 -0500

    Theme paths update

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     content: [
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
+        './resources/views/forms/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
 }
